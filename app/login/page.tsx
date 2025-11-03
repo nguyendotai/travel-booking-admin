c6ff00dc02dd7f3e7f505cfa328990fe6015d8f1
nguyendotai <nguyendotai11@gmail.com>
2025-11-04 03:59:34 +0700

    update .env

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,13 +7,14 @@ export default function LoginPage() {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const router = useRouter();
+  const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
     setError("");
 
     try {
-      const res = await fetch("https://travel-booking-backend-production.up.railway.app//api/auth/admin/login", {
+      const res = await fetch(`${BASE_URL}/api/auth/admin/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
